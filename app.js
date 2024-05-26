009e4ae316349432e797fb2f22d11a040e9e4d7e
lucas-gruber <blaumc@icloud.com>
2024-05-26 15:08:44 +0200

    refactor: Remove uploadRouter from app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index')
 var convertRouter = require('./routes/convert')
-var uploadRouter = require('./routes/upload')
 
 var app = express()
 
@@ -21,7 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/convert', convertRouter)
-app.use('/upload', uploadRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
