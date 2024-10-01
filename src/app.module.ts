fdf78e9dff04e9d2b3d7bf7ac544ae90e769829f
lucas-gruber <blaumc@icloud.com>
2024-10-01 18:36:54 +0200

    feat: added ping route

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UpdateController } from './shipapi/update/update.controller';
 import { VideoController } from './video/video.controller';
 import { NextController } from './shipapi/update/next/next.controller';
+import { PingController } from './ping/ping.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UpdateController, VideoController, NextController],
+  controllers: [AppController, UpdateController, VideoController, NextController, PingController],
   providers: [AppService],
 })
 export class AppModule {}
